d8b8a79dbab19bde1f45d77a07881e03f4cffe61
Vieri <vescamilla1@appdev.org>
2024-10-21 15:19:39 -0400

    created hello.py

---
+++ hello.py
@@ -0,0 +1,19 @@
+print('Hello World')
+
+
+message = 'Hello World'
+print(message)
+
+
+dollars = 3
+cents = .50
+
+
+print(type(dollars + cents))
+
+dollars = '3'
+
+print(dollars + "dollars")
+print(message)
+
+print('3 dollars' + ' ' 'fifty cents')
