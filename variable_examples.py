74bbf6901c43ebfada3624eb160df09b2fadf86d
Vieri <vescamilla1@appdev.org>
2024-10-21 16:21:38 -0400

    Updated assignment with comments

---
+++ variable_examples.py
@@ -0,0 +1,30 @@
+CustomerID = 1
+
+FirstName = 'Zack'
+
+MiddleName = 'Andre'
+
+LastName = 'Wilson'
+
+Gender = 'Male'
+
+DOB = '01/16/2001'
+
+LPN = 123456
+
+AutoPolicyNum = 111
+
+
+
+# For the variables that needed numbers I usually put integers, but the date is one i added as a string because im not too sure if this program requires a special statemen for dates. #
+
+
+# Here I am testing to see if my code works with the variables I set # 
+print(FirstName + ' ' + Gender)
+
+
+City = 'Chicago'
+
+State = 'Illinois'
+
+print(City + ', ' + State )
