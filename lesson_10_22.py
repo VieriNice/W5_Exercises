5ec31cb25dc09f030ca9451e0c79f8d254e15b35
Vieri <vescamilla1@appdev.org>
2024-10-22 15:15:18 -0400

    created tips.py

--- lesson_10_22.py
+++ lesson_10_22.py
@@ -1,13 +1,18 @@
 fruits = 16
-
-
 vegetables = 12
+herbs = '11'
+
+
 
 
 produce = fruits + vegetables #16 + 12
 
+produce += int(herbs)
 
-print(produce)
+
+print(produce) # 28
+
+print("Fruits: " + str(fruits))
 
 
 fruits = (fruits -10) + (fruits +15) # fruit = 6 + 31 =37
@@ -20,5 +25,4 @@ produce = fruits + vegetables
 
 
 print(produce)
-
 
