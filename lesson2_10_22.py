56b8e2e3c0d156242c8f9c25e724c563d5b97d70
Vieri <vescamilla1@appdev.org>
2024-10-22 15:15:46 -0400

    created lesso2

---
+++ lesson2_10_22.py
@@ -0,0 +1,28 @@
+import math
+
+depth = 415
+
+yards_deep_origi = depth / 3
+
+print(yards_deep_origi)
+
+yards_deep = 415
+yards_deep /=  2 + 1
+
+print(yards_deep)
+
+# depth -= 1 
+
+# yards_deep2 = depth / 3
+
+# print(yards_deep2)
+
+# print(3 * 'bob ')
+
+print(3 * float('3'))
+
+print(int(False))
+
+print(bool(1.0))
+
+print(math.pi)
