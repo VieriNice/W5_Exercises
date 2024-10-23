e3a67830294d1dfbafa40b9f76dfaac9f6e332ca
Vieri <vescamilla1@appdev.org>
2024-10-23 14:35:16 -0400

    created c_to_f.py

---
+++ MathScripts/c_to_f.py
@@ -0,0 +1,6 @@
+# 2. Convert fahrenheit
+
+celcius_temp = 25
+celcius_to_fahrenheit = (25 * (9/5)) + 32
+
+print("The temperature from " + str(celcius_temp) + " celcius to fahrenhit is " + str(celcius_to_fahrenheit) + " fahrenheit. ")
