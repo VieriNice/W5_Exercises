1cec9ad227416e50c0d476429135a9463242d444
Vieri <vescamilla1@appdev.org>
2024-10-23 14:31:27 -0400

    created f_to_c.py

---
+++ MathScripts/f_to_c.py
@@ -0,0 +1,6 @@
+# 1. How to convert a temperature from Fahrenheit to Celcius?
+
+fahrenheit_temp = 68
+fahrenheit_to_celcius_formula = (68-32) * (5/9)
+
+print("The temperature from " + str(fahrenheit_temp) + " fahrenheit to celcius is " + str(fahrenheit_to_celcius_formula) + " celcius. ")
