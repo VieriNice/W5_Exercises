d04a4f01401d630d9a53f5b10f895e4058c33ab3
Vieri <vescamilla1@appdev.org>
2024-10-23 14:51:19 -0400

    created taxes.py

---
+++ MathScripts/taxes.py
@@ -0,0 +1,10 @@
+# 3. Federal taxes are 23% of your salary every month. You make X amount of money. How much is withheld for taxes? #
+
+federal_taxes = .23
+employee_wage = 60000
+
+tax_withheld = 60000 * federal_taxes
+
+# print(tax_withheld) - used this to test the formula created 
+print("The federal tax withheld is $" + str((tax_withheld)))
+
