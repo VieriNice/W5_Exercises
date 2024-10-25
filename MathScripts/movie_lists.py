18d3b713b33a8ca5e4ca0ad3f10c2aeb0ce00def
Vieri <vescamilla1@appdev.org>
2024-10-25 09:59:35 -0400

    created movie_lists.py

---
+++ MathScripts/movie_lists.py
@@ -0,0 +1,12 @@
+
+
+movies = ['Her', 'La La Land', '500 Days of Summer', 'Crazy Stupid Love']
+print("The list "+ str(movies) + " includes my top " + str(len(movies)) + " favorite movies." )
+print(movies)
+
+
+print(sorted(movies))
+sorted(movies)
+
+movies.sort() #this puts the movies in alphabetical order
+print(movies)
