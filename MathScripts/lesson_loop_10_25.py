7323b6c6388bdbae24db00475e8a596347aca9f2
Vieri <vescamilla1@appdev.org>
2024-10-27 17:54:18 -0400

    create loop_10_25.py

---
+++ MathScripts/lesson_loop_10_25.py
@@ -0,0 +1,57 @@
+# Types of iterables:
+
+str_i = 'string'
+list = [1,2,3,4]
+tuple  = ('allspice', 'bay', 'cinamon', 'dandelion')
+range_5 = range(1,7)
+dict1 = {'1':'allspice', '2':'bay', '3':'cinamon', '4':'dandelion'}
+
+sav_balance = 100
+check_balance = 50
+print(f'Starting balance is {sav_balance} and {check_balance}')
+
+while sav_balance > 50:
+    print('Treat Yourself')
+    check_balance -= 25
+    print(f'New checking balance is {check_balance}')
+    if check_balance < 50:
+        break
+
+print(f'Your final balance are {sav_balance} and {check_balance}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in tuple:
+#     counter += 1 
+#     if i == 'bay' or i == 'dandelion':
+#         print('Bay is a leave.')
+#     else:
+#         spices.append(i)
+#         print(f'{i.capitalize()} is a spice')
+
+# print(i)
+
+# print(f'We have {counter} items in our spice cabinet.')
+# print(f'{spices} are spices')
+
+
+
