6dbfece0acb95f2be9d1b630d73eeda8b47ffa5c
Vieri <vescamilla1@appdev.org>
2024-10-28 15:29:45 -0400

    created show_major.py

---
+++ ifscripts/show_major.py
@@ -0,0 +1,7 @@
+student_name = str(input("Enter student name: "))
+student_major = str(input("Enter student maor: "))
+
+major_code = ['BIO', 'CSCI', 'HIST', 'MKT']
+major_name = ['Biology', 'Computer Science', 'History', 'Marketing'] 
+
+
