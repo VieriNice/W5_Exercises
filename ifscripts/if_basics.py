498829bc65be1523b5106c9079ed4ba5e0f25020
Vieri <vescamilla1@appdev.org>
2024-10-27 23:52:19 -0400

    created if_basics.py

---
+++ ifscripts/if_basics.py
@@ -0,0 +1,33 @@
+
+
+x = 100
+y = 20 
+
+if x / y == 5:
+    print('x divided by y is 5')
+    x = 1
+else: 
+    print('Are the variables set up correctly?')
+
+
+if x * y == y:
+    print('now x times y is y')
+    x = 10 
+else: 
+    print("Whoops, x equals " + str(x))
+
+
+if x < y: 
+    print('x is less than y')
+    x ** 2
+else: 
+    print('uh oh, x is not less then y')
+
+
+if x > y: 
+    print('how is x greater than y')
+else: 
+    print('x is NOT greater than y')
+
+print('The final value of x is  and the final value of y is')
+
