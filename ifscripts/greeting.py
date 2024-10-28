82ef277924253011138fd6ae1bdc223bd3981359
Vieri <vescamilla1@appdev.org>
2024-10-28 11:49:25 -0400

    finished greeting.py

--- ifscripts/greeting.py
+++ ifscripts/greeting.py
@@ -1,4 +1,11 @@
 
 time_inserted = int(input('Enter the time you are starting your shift: '))
 
-if (time_inserted > 12 )
+if (time_inserted < 10 ): 
+    print("Good Morning")
+elif 10 <= time_inserted < 17: 
+    print('Good Day!')
+else: 
+    print('Good evening')
+    
+    
