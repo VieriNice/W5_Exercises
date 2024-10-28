910db328a8efba7b7c4be86065c6ee41301b783c
Vieri <vescamilla1@appdev.org>
2024-10-28 15:09:43 -0400

    finished min_max.py

---
+++ ifscripts/min_max.py
@@ -0,0 +1,11 @@
+a = int(input('Enter value for a: '))
+b = int(input('Enter value for b: '))
+c = int(input('Enter value for c: '))
+
+
+if a >= 10 or 12 > b or 12 < c:
+    print('Nice!')
+else:
+    print('Womp womp.')
+
+
