e34131d45c623ba9a2b9c61bf2d395a87c2f15d7
Vieri <vescamilla1@appdev.org>
2024-10-29 01:05:00 -0400

    created saving_goals

---
+++ LoopScripts/saving_goals.py
@@ -0,0 +1,7 @@
+checkings_account = 1000
+weekly_sav_ammount = 100
+sav_goal = 5000
+
+while checkings_account < 5000:
+    checkings_account += 1
+    print("you are almost there! you currently have: " + str(checkings_account))
